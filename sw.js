9618699a76b9d2e93f5d961206cccc98faf957be
silviademiguel7 <silviademiguel7@gmail.com>
2022-06-10 11:41:23 +0200

    initial commit

---
+++ sw.js
@@ -0,0 +1,64 @@
+// imports
+importScripts("js/sw-utils.js");
+
+const STATIC_CACHE = "static-v1";
+const DYNAMIC_CACHE = "dinamyc-v1";
+const INMUTABLE_CHACE = "inmutable-v1";
+
+const APP_SHELL = [
+  "/",
+  "index.html",
+  "css/style.css",
+  "img/favicon.ico",
+  "img/avatars/spiderman.jpg",
+  "img/avatars/thor.jpg",
+  "img/avatars/wolverine.jpg",
+  "img/avatars/hulk.jpg",
+  "img/avatars/ironman.jpg",
+  "js/app.js",
+  "js/sw-utils.js",
+];
+
+const APP_SHELL_INMUTABLE = [
+  "https://fonts.googleapis.com/css?family=Quicksand:300,400",
+  "https://fonts.googleapis.com/css?family=Lato:400,300",
+  "https://use.fontawesome.com/releases/v5.3.1/css/all.css",
+  "css/animate.css",
+  "js/libs/jquery.js",
+];
+
+self.addEventListener("install", (e) => {
+  const cacheStatic = caches
+    .open(STATIC_CACHE)
+    .then((cache) => cache.addAll(APP_SHELL));
+  const cacheInmutable = caches
+    .open(INMUTABLE_CHACE)
+    .then((cache) => cache.addAll(APP_SHELL_INMUTABLE));
+  e.waitUntil(Promise.all([cacheStatic, cacheInmutable]));
+});
+
+self.addEventListener("activate", (e) => {
+  const respuesta = caches.keys().then((keys) => {
+    keys.forEach((key) => {
+      if (key !== STATIC_CACHE && key.includes("static")) {
+        return caches.delete(key);
+      }
+    });
+  });
+
+  e.waitUntil(respuesta);
+});
+
+self.addEventListener("fectch", (e) => {
+  const respuesta = caches.match(e.request).then((res) => {
+    if (res) {
+      return res;
+    } else {
+      return fetch(e.request).then((newResp) => {
+        return actualizaCacheDinamico(DYNAMIC_CACHE, e.request, newResp);
+      });
+    }
+  });
+
+  e.respondWith(respuesta);
+});
